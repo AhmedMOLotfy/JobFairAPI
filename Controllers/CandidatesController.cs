f7a109a1681e1b13790a2f2d574d3d6fb454d6ac
Ahmed Lotfy <programmerahmedlotfy@gmail.com>
2023-10-10 02:48:42 +0200

    Creating API controller , and applying asynchronos code

---
+++ Controllers/CandidatesController.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JobFairAPI.Data;
+using JobFairAPI.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobFairAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] // api/candidates
+    public class CandidatesController : ControllerBase
+    {
+        private readonly DataContext _context;
+        
+        public CandidatesController(DataContext context){
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CandidatesEntity>>> GetCandidates(){
+            var users = await _context.Candidates.ToListAsync();
+            return users;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CandidatesEntity>> GetCandidate(int id){
+            var user = await _context.Candidates.FindAsync(id);
+            return user;
+        }
+
+    }
+
+    
+
+}
