c3e25c9aa542e476d2126bf8e0d37967ce5b93ef
Ahmed Lotfy <programmerahmedlotfy@gmail.com>
2023-10-10 20:41:04 +0200

    Solveing CORS problem

--- Program.cs
+++ Program.cs
@@ -12,8 +12,10 @@ builder.Services.AddDbContext<DataContext>(
         opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
     }
 );
+builder.Services.AddCors();
 
 var app = builder.Build();
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 
 // Configure the HTTP request pipeline.
