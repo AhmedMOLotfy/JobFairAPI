0601eef75a626a7040bd46df15fc80184f5d8d35
Ahmed Lotfy <programmerahmedlotfy@gmail.com>
2023-10-11 04:17:24 +0200

    Using Extensions and formatting docs

--- Controllers/BaseApiController.cs
+++ Controllers/BaseApiController.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
 namespace JobFairAPI.Controllers
@@ -10,6 +6,6 @@ namespace JobFairAPI.Controllers
     [Route("api/[controller]")]
     public class BaseApiController : ControllerBase
     {
-        
+
     }
 }
--- DTOs/UserDto.cs
+++ DTOs/UserDto.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace JobFairAPI.DTOs
 {
     public class UserDto
